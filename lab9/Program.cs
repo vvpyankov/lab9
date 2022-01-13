59941a8f99e22ed6ac7a75215d940b1ccbbcc51c
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-01-13 22:25:55 +0300

    Добавьте файлы проекта.

---
+++ lab9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+Console.WriteLine(a / b);
+            }
+            catch (DivideByZeroException ex) when (a==0 && b==0)
+            {
+
+                Console.WriteLine("деление 0 на 0"); ;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message); ;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+ 
---
+++ lab9zadacha1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int[] mathSymb = { 1, 2, 3, 4 };
+            Console.WriteLine("Перед Вами простейший калькулятор!");
+            try
+            {
+                Console.Write("Введите целое число: A = ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите целое число: B = ");
+                b = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine(@"Введите код операции:
+    1 - сложение
+    2 - вычитание
+    3 - произведение
+    4 - частное");
+            Console.Write("Выш выбор: ");
+
+
+            int numberSymbol = 0;
+
+            try
+            {
+                numberSymbol = Convert.ToInt32(Console.ReadLine());
+                int symbol = mathSymb[-1 + numberSymbol];
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка. Некорректный ввод.");
+                return;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Ошибка. Нет операции с выбранным номером.");
+                return;
+            }
+
+            double action = 0;
+            try
+            {
+                if (numberSymbol == 1)
+                {
+                    action = a + b;
+                }
+                else if (numberSymbol == 2)
+                {
+                    action = a - b;
+                }
+                else if (numberSymbol == 3)
+                {
+                    action = a * b;
+                }
+                else if (numberSymbol == 4)
+                {
+                    action = a / b;
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return;
+            }
+
+            Console.WriteLine("Результат = {0}", action);
+            Console.ReadKey();
+        }
+    }
+}
